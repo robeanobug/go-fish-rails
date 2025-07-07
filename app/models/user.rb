e08a600bd2a4744daaa6ec8a761cb33bfa4121db
robeanobug <fakeemail@gmail.com>
2025-07-07 17:17:52 -0400

    add comments to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # has_many :game_users
+  # has_many :games, through: :game_users
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
