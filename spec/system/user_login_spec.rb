c2a30d1984041ab0d55878fe2839b527ed19e1ce
robeanobug <fakeemail@gmail.com>
2025-07-08 10:10:33 -0400

    create tests for create game, destroy game, show game

--- spec/system/user_login_spec.rb
+++ spec/system/user_login_spec.rb
@@ -1,22 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe "User", type: :system, chrome: true do
+  let!(:user) { create(:user) }
+  let(:game) { build(:game) }
+
+  def load_index
+    sign_in user
+    visit "/"
+  end
+  
+  def create_game
+    find(".btn", text: "New Game").click
+    expect(page).to have_text("Player count")
+    fill_in "game[name]", with: game.name
+    fill_in "game[player_count]", with: game.player_count
+    click_on "Create game"
+  end
+
   describe "sign up" do
-    let(:user) { build(:user) }
+    let(:build_user) { build(:user) }
     it 'signs up a user' do
       visit new_user_registration_path
       expect(page).to have_text("Sign up")
 
-      fill_in "user_email", with: user.email
-      fill_in "user_password", with: user.password
-      fill_in "user_password_confirmation", with: user.password
+      fill_in "user_email", with: build_user.email
+      fill_in "user_password", with: build_user.password
+      fill_in "user_password_confirmation", with: build_user.password
       click_on "Sign up"
       expect(page).to have_text("Go Fish")
     end
   end
 
   describe "login" do
-    let!(:user) { create(:user) }
 
     def login_user
       visit "/"
@@ -40,9 +55,41 @@ RSpec.describe "User", type: :system, chrome: true do
       expect(page).to have_text("Go Fish")
     end
   end
+  describe "user create game" do
+    before do
+      load_index
+    end
+
+    it 'can create a game' do
+      create_game
+      expect(page).to have_text(game.name)
+    end
+  end
   
-  fit 'can create a game' do
-    find(".btn", text: "New Game").click
-    expect(page).to have_text("Player count")
+  describe "user destroy game" do
+    before do
+      load_index
+      create_game
+    end
+    
+    it 'can destroy a game' do
+      expect(page).to have_text(game.name)
+      click_on "Delete", match: :first
+      expect(page).to have_no_text(game.name)
+    end
+  end
+
+  describe "showing a game" do
+    before do
+      load_index
+      create_game
+    end
+
+    it 'shows a game' do
+      expect(page).to have_text(game.name)
+      click_on game.name
+      expect(page).to have_text(game.name)
+      expect(page).to have_text("Back to games")
+    end
   end
 end
