497b520a7ec0e002d3fd9ed77e932175b75c7cfa
robeanobug <fakeemail@gmail.com>
2025-07-08 12:52:57 -0400

    run rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_08_104102) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_08_165129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "game_users", force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_game_users_on_game_id"
+    t.index ["user_id"], name: "index_game_users_on_user_id"
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "name", null: false
@@ -30,4 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_08_104102) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "game_users", "games"
+  add_foreign_key "game_users", "users"
 end
