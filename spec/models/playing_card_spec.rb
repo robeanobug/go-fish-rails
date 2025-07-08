ac0c46b90f1ec61994fd0d98d89132c4d64289a1
robeanobug <fakeemail@gmail.com>
2025-07-08 16:07:59 -0400

    add playing card

--- spec/models/playing_card_spec.rb
+++ spec/models/playing_card_spec.rb
@@ -1,5 +1,9 @@
 require "rails_helper"
 
 RSpec.describe PlayingCard do
-  
+  let(:playing_card) { PlayingCard.new('2', 'Spades')}
+  fit 'has a rank and a suit' do
+    expect(playing_card.rank).to eq('2')
+    expect(playing_card.suit).to eq('Spades')
+  end
 end
