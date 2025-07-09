479218aef05201edcf0ffa95f27be18ad2abb7c2
robeanobug <fakeemail@gmail.com>
2025-07-09 08:39:53 -0400

    add player and player spec

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,39 @@
 class Player
-  
+  attr_reader :name
+  attr_accessor :hand, :books
+
+  BASE_HAND_SIZE = 7
+  SMALL_HAND_SIZE = 5
+  BOOK_LENGTH = 4
+
+  def initialize(name, hand = [], books = [])
+    @name = name
+    @hand = hand
+    @books = books
+  end
+
+  def self.dump(obj)
+    obj.as_json
+  end
+
+  def self.load(json)
+    return nil if json.blank?
+    self.from_json(json)
+  end
+
+  def self.from_json(json)
+    hand = json['hand'].map { |card_hash| PlayingCard.new(**card_hash.symbolize_keys) }
+    books = json['books'].map do |book|
+      book.map { |card_hash| PlayingCard.new(**card_hash.symbolize_keys) }
+    end
+    self.new(json['name'], hand, books)
+  end
+
+  def as_json(*)
+    {
+      name: name,
+      hand: hand.map(&:as_json),
+      books: books.map { |book|  book.map(&:as_json) },
+    }.stringify_keys
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,40 @@
 require "rails_helper"
 
 RSpec.describe Player do
-  
+  let(:player) { Player.new('Player 1') }
+  let(:player_hash) { Player.dump(player) }
+  let(:ace_spades) { PlayingCard.new(rank: 'Ace', suit: 'Spades') }
+  let(:ace_hearts) { PlayingCard.new(rank: 'Ace', suit: 'Hearts') }
+  let(:ace_diamonds) { PlayingCard.new(rank: 'Ace', suit: 'Diamonds') }
+  let(:ace_clubs) { PlayingCard.new(rank: 'Ace', suit: 'Clubs') }
+  let(:king_spades) { PlayingCard.new(rank: 'King', suit: 'Spades') }
+  let(:king_hearts) { PlayingCard.new(rank: 'King', suit: 'Hearts') }
+  let(:king_diamonds) { PlayingCard.new(rank: 'King', suit: 'Diamonds') }
+  let(:king_clubs) { PlayingCard.new(rank: 'King', suit: 'Clubs') }
+
+  before do
+    player.hand = [ace_spades, ace_hearts]
+    player.books = [[king_spades, king_hearts, king_diamonds, king_clubs]]
+  end
+  it 'has a name' do
+    expect(player.name).to eq('Player 1')
+  end
+  it 'has a hand' do
+    expect(player.hand).to be_a(Array)
+  end
+  it 'has books' do
+    expect(player.books).to be_a(Array)
+  end
+  it 'should create a hash' do
+    expect(player_hash).to be_a Hash
+    expect(player_hash).to have_key('name')
+    expect(player_hash).to have_key('hand')
+    expect(player_hash).to have_key('books')
+  end
+  it 'should return object from json' do
+    player = Player.load(player_hash)
+    expect(player.name).to be_a String
+    expect(player.hand).to be_a Array
+    expect(player.books).to be_a Array
+  end
 end
