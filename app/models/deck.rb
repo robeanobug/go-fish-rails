91f3bc90262a651ffc3678ef0f51849ba6ebe607
robeanobug <fakeemail@gmail.com>
2025-07-09 08:39:39 -0400

    add deck and deck spec

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,23 @@
 class Deck
-  
+  attr_accessor :cards
+  DECK_COUNT = 52 
+  def initialize(cards = build_deck.shuffle)
+    @cards = cards
+  end
+
+  def build_deck
+    PlayingCard::RANKS.map do |rank|
+      PlayingCard::SUITS.map do |suit|
+        PlayingCard.new(rank: rank, suit: suit)
+      end
+    end.flatten
+  end
+
+  def deal_card
+    cards.pop
+  end
+
+  def shuffle!
+    cards.shuffle!
+  end
 end
--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
 
 RSpec.describe Deck do
-  
+  let(:deck) { Deck.new }
+  it 'has a specified deck length' do
+    expect(deck.cards.length).to eq Deck::DECK_COUNT
+  end
+  it 'holds playing cards' do
+    expect(deck.cards.first).to be_a(PlayingCard)
+  end
+  it 'deals a card' do
+    expect(deck.deal_card).to be_a(PlayingCard)
+  end
+  it 'should shuffle the deck' do
+    unshuffled_deck = deck.cards.dup
+    deck.shuffle!
+    expect(unshuffled_deck).to match_array deck.cards
+    expect(unshuffled_deck).to_not eq deck.cards
+  end
 end
