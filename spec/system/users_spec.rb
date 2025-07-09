8703ca9e6f80fc9a03fc07552d89e981a8ac82ef
robeanobug <fakeemail@gmail.com>
2025-07-09 10:43:03 -0400

    change user_login_spec name to users spec

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "User", type: :system, chrome: true do
+RSpec.describe "Users", type: :system do
   let!(:user) { create(:user) }
   let(:game) { build(:game) }
 
