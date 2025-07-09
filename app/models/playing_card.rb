8a0e97150608c8122d982f15dfef00686bde2ea5
robeanobug <fakeemail@gmail.com>
2025-07-09 08:40:05 -0400

    add playing card and playing card spec

--- app/models/playing_card.rb
+++ app/models/playing_card.rb
@@ -1,9 +1,9 @@
 class PlayingCard
   attr_reader :rank, :suit
-  RANKS = %w[2 3 4 5 6 7 8 9 10 Jack Queen King Ace]
+  RANKS = %w[Two Three Four Five Six Seven Eight Nine Ten Jack Queen King Ace]
   SUITS = %w[Spades Hearts Diamonds Clubs]
 
-  def initialize(rank, suit)
+  def initialize(rank:, suit:)
     @rank = rank
     @suit = suit
   end
--- spec/models/playing_card_spec.rb
+++ spec/models/playing_card_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe PlayingCard do
-  let(:playing_card) { PlayingCard.new('2', 'Spades')}
-  fit 'has a rank and a suit' do
-    expect(playing_card.rank).to eq('2')
+  let(:playing_card) { PlayingCard.new(rank: 'Two', suit: 'Spades')}
+  it 'has a rank and a suit' do
+    expect(playing_card.rank).to eq('Two')
     expect(playing_card.suit).to eq('Spades')
   end
 end
