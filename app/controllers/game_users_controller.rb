a1f210ea8f778b0c850b705ae761fd2300713af8
robeanobug <fakeemail@gmail.com>
2025-07-10 11:16:36 -0400

    fix game users controller to follow rails conventional create function

--- app/controllers/game_users_controller.rb
+++ app/controllers/game_users_controller.rb
@@ -1,19 +1,9 @@
 class GameUsersController < ApplicationController
   def create
-    # @game = Game.find(params[:format])
-    # current_user.games << @game
-    # binding.irb
-    # @game.users << current_user
-    
-    # @game.start!
-    # redirect_to @game
-
     @game_user = GameUser.new(game_user_params)
-    @game = Game.find(params[:game_id])
 
-    if !@game.users.include?(@game_user) && @game.users.count < @game.player_count
-      @game.users << @game_user.user
-      @game.start!
+    if @game_user.save
+      @game_user.game.start_if_ready!
 
       redirect_to @game_user.game
     else
