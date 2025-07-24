1a360655657dcae2263dfb6096362654cac1936c
robeanobug <fakeemail@gmail.com>
2025-07-24 10:23:16 -0400

    add sort to leaderboard

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,6 +1,7 @@
 class StatsController < ApplicationController
   def show
     @q = Leaderboard.ransack(params[:q])
+    @q.sorts = 'id asc' if @q.sorts.empty?
     @leaderboards = @q.result.page params[:page]
   end
 end
--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -3,7 +3,7 @@ class Leaderboard < ApplicationRecord
   paginates_per 20
 
   def self.ransackable_attributes(auth_object = nil)
-    [ 'user_id', 'time_played', 'total_games', 'username', 'won_games' ]
+    [ 'id', 'username', 'games_won', 'games_lost', 'total_games', 'percent', 'time_in_game' ]
   end
 
   def readonly?
